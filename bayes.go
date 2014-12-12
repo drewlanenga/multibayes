4a15a5aecbab1173f5471b565ddb1738502b469e
allisonmorgan <allison.morgan@gmail.com>
2014-12-12 13:37:11 -0800

    Working on implementing tf-idf

--- bayes.go
+++ bayes.go
@@ -10,17 +10,30 @@ var (
 )
 
 type Classifier struct {
-	Tokenizer *tokenizer    `json:"-"`
-	Matrix    *sparseMatrix `json:"matrix"`
+	Tokenizer *tokenizer   `json:"-"`
+	Matrix    sparseMatrix `json:"matrix"`
 }
 
 // Create a new multibayes classifier.
-func NewClassifier() *Classifier {
+func NewClassifierMap() *Classifier {
 	tokenize, _ := newTokenizer(&tokenizerConf{
 		NGramSize: 1,
 	})
 
-	sparse := newSparseMatrix()
+	sparse := newSparseMatrixMap()
+
+	return &Classifier{
+		Tokenizer: tokenize,
+		Matrix:    sparse,
+	}
+}
+
+func NewClassifierInt() *Classifier {
+	tokenize, _ := newTokenizer(&tokenizerConf{
+		NGramSize: 1,
+	})
+
+	sparse := newSparseMatrixInt()
 
 	return &Classifier{
 		Tokenizer: tokenize,
@@ -40,8 +53,8 @@ func (c *Classifier) Posterior(document string) map[string]float64 {
 	tokens := c.Tokenizer.Parse(document)
 	predictions := make(map[string]float64)
 
-	for class, classcolumn := range c.Matrix.Classes {
-		if len(classcolumn.Data) < minClassSize {
+	for class, classcolumn := range c.Matrix.getClasses() {
+		if len(classcolumn.getData()) < minClassSize {
 			continue
 		}
 
@@ -49,22 +62,22 @@ func (c *Classifier) Posterior(document string) map[string]float64 {
 		smoothN := n + (smoother * 2)
 
 		priors := []float64{
-			float64(n+smoother) / float64(c.Matrix.N+(smoother*2)),            // P(C=Y)
-			float64(c.Matrix.N-n+smoother) / float64(c.Matrix.N+(smoother*2)), // P(C=N)
+			float64(n+smoother) / float64(c.Matrix.getN()+(smoother*2)),                 // P(C=Y)
+			float64(c.Matrix.getN()-n+smoother) / float64(c.Matrix.getN()+(smoother*2)), // P(C=N)
 		}
 
 		loglikelihood := []float64{1.0, 1.0}
 
 		// check if each token is in our token sparse matrix
 		for _, token := range tokens {
-			if tokencolumn, ok := c.Matrix.Tokens[token.String()]; ok {
+			if tokencolumn, ok := c.Matrix.getTokens()[token.String()]; ok {
 				// conditional probability the token occurs for the class
-				joint := intersection(tokencolumn.Data, classcolumn.Data)
+				joint := intersection(tokencolumn.getData(), classcolumn.getData())
 				conditional := float64(joint+smoother) / float64(smoothN) // P(F|C=Y)
 				loglikelihood[0] += math.Log(conditional)
 
 				// conditional probability the token occurs if the class doesn't apply
-				not := len(tokencolumn.Data) - joint
+				not := len(tokencolumn.getData()) - joint
 				notconditional := float64(not+smoother) / float64(smoothN) // P(F|C=N)
 				loglikelihood[1] += math.Log(notconditional)
 			}
@@ -102,14 +115,32 @@ func bayesRule(prior, likelihood []float64) []float64 {
 	return posterior
 }
 
+// define intersection function that takes interfaces
+func intersection(sparseColumn1, sparseColumn2 sparseColumn) int {
+	var count int
+	return count
+}
+
 // elements that are in both array1 and array2
-func intersection(array1, array2 []int) int {
+func arrayIntersection(array1, array2 []int) int {
 	var count int
 	for _, elem1 := range array1 {
 		for _, elem2 := range array2 {
 			if elem1 == elem2 {
 				count++
 				break
+			}
+		}
+	}
+	return count
+}
+
+func mapIntersection(map1, map2 map[int]int) int {
+	var count int
+	for index1, count1 := range map1 {
+		for index2, count2 := range map2 {
+			if index1 == index2 {
+				count = count + (count1 + count2)
 			}
 		}
 	}
--- sparse.go
+++ sparse.go
@@ -1,32 +1,37 @@
 package multibayes
 
-type sparseMatrix struct {
-	Tokens  map[string]*sparseColumn `json:"tokens"`  // []map[tokenindex]occurence
-	Classes map[string]*sparseColumn `json:"classes"` // map[classname]classindex
-	N       int                      `json:"n"`       // number of rows currently in the matrix
+type sparseColumn interface {
+	Add(int)
+	Count() int
+	Expand(int) []float64
+	getData()
 }
 
-type sparseColumn struct {
+type sparseColumnInt struct {
 	Data []int `json:"data"`
 }
 
-func newSparseColumn() *sparseColumn {
-	return &sparseColumn{
+type sparseColumnMap struct {
+	Data map[int]int `json:"data"`
+}
+
+func newSparseColumnInt() *sparseColumnInt {
+	return &sparseColumnInt{
 		Data: make([]int, 0, 1000),
 	}
 }
 
-func (s *sparseColumn) Add(index int) {
+func (s *sparseColumnInt) Add(index int) {
 	s.Data = append(s.Data, index)
 }
 
 // return the number of rows that contain the column
-func (s *sparseColumn) Count() int {
+func (s *sparseColumnInt) Count() int {
 	return len(s.Data)
 }
 
 // sparse to dense
-func (s *sparseColumn) Expand(n int) []float64 {
+func (s *sparseColumnInt) Expand(n int) []float64 {
 	expanded := make([]float64, n)
 	for _, index := range s.Data {
 		expanded[index] = 1.0
@@ -34,21 +39,69 @@ func (s *sparseColumn) Expand(n int) []float64 {
 	return expanded
 }
 
-func newSparseMatrix() *sparseMatrix {
-	return &sparseMatrix{
-		Tokens:  make(map[string]*sparseColumn),
-		Classes: make(map[string]*sparseColumn),
+func (s *sparseColumnInt) getData() []int {
+	return s.Data
+}
+
+func newSparseColumnMap() *sparseColumnMap {
+	return &sparseColumnMap{
+		Data: make(map[int]int),
+	}
+}
+
+func (s *sparseColumnMap) Add(index int) {
+	s.Data[index]++
+}
+
+func (s *sparseColumnMap) Count() int {
+	count := 0
+	for _, val := range s.Data {
+		count := count + val
+	}
+	return count
+}
+
+func (s *sparseColumnMap) Expand(n int) []float64 {
+	expanded := make([]float64, n)
+	for index, count := range s.Data {
+		expanded[index] = float64(count)
+	}
+	return expanded
+}
+
+func (s *sparseColumnMap) getData() map[int]int {
+	return s.Data
+}
+
+type sparseMatrix interface {
+	Add([]ngram, []string)
+	getTokens() map[string]sparseColumn
+	getClasses() map[string]sparseColumn
+	getN() int
+}
+
+type sparseMatrixMap struct {
+	Tokens  map[string]*sparseColumnMap `json:"tokens"`  // []map[tokenindex]occurence
+	Classes map[string]*sparseColumnInt `json:"classes"` // map[classname]classindex
+	N       int                         `json:"n"`       // number of rows currently in the matrix
+}
+
+func newSparseMatrixMap() sparseMatrix {
+	return sparseMatrixMap{
+		Tokens:  make(map[string]*sparseColumnMap),
+		Classes: make(map[string]*sparseColumnInt),
 		N:       0,
 	}
 }
 
-func (s *sparseMatrix) Add(ngrams []ngram, classes []string) {
+func (s *sparseMatrixMap) Add(ngrams []ngram, classes []string) {
 	if len(ngrams) == 0 || len(classes) == 0 {
 		return
 	}
 	for _, class := range classes {
 		if _, ok := s.Classes[class]; !ok {
-			s.Classes[class] = newSparseColumn()
+			// sparse column int here
+			s.Classes[class] = newSparseColumnInt()
 		}
 
 		s.Classes[class].Add(s.N)
@@ -57,11 +110,76 @@ func (s *sparseMatrix) Add(ngrams []ngram, classes []string) {
 	for _, ngram := range ngrams {
 		gramString := ngram.String()
 		if _, ok := s.Tokens[gramString]; !ok {
-			s.Tokens[gramString] = newSparseColumn()
+			// sparse column map here
+			s.Tokens[gramString] = newSparseColumnMap()
 		}
 
 		s.Tokens[gramString].Add(s.N)
 	}
 	// increment the row counter
 	s.N++
+}
+
+func (s *sparseMatrixMap) getTokens() map[string]*sparseColumnMap {
+	return s.Tokens
+}
+
+func (s *sparseMatrixMap) getClasses() map[string]*sparseColumnMap {
+	return s.Classes
+}
+
+func (s *sparseColumnMap) getN() int {
+	return s.N
+}
+
+type sparseMatrixInt struct {
+	Tokens  map[string]*sparseColumnInt `json:"tokens"`  // []map[tokenindex]occurence
+	Classes map[string]*sparseColumnInt `json:"classes"` // map[classname]classindex
+	N       int                         `json:"n"`       // number of rows currently in the matrix
+}
+
+func newSparseMatrixInt() sparseMatrix {
+	return sparseMatrixInt{
+		Tokens:  make(map[string]*sparseColumnInt),
+		Classes: make(map[string]*sparseColumnInt),
+		N:       0,
+	}
+}
+
+func (s *sparseMatrixInt) Add(ngrams []ngram, classes []string) {
+	if len(ngrams) == 0 || len(classes) == 0 {
+		return
+	}
+	for _, class := range classes {
+		if _, ok := s.Classes[class]; !ok {
+			// sparse column int here
+			s.Classes[class] = newSparseColumnInt()
+		}
+
+		s.Classes[class].Add(s.N)
+	}
+
+	for _, ngram := range ngrams {
+		gramString := ngram.String()
+		if _, ok := s.Tokens[gramString]; !ok {
+			// sparse column map here
+			s.Tokens[gramString] = newSparseColumnInt()
+		}
+
+		s.Tokens[gramString].Add(s.N)
+	}
+	// increment the row counter
+	s.N++
+}
+
+func (s *sparseMatrixInt) getTokens() map[string]*sparseColumnInt {
+	return s.Tokens
+}
+
+func (s *sparseMatrixInt) getClasses() map[string]*sparseColumnInt {
+	return s.Classes
+}
+
+func (s *sparseColumnInt) getN() int {
+	return s.N
 }
